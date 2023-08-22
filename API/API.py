c5ca90eb6933ace0402cdd3e186125aef5fe7d6f
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-08-22 21:36:29 +0200

    Update API.py

--- API/API.py
+++ API/API.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import logging
 from zipfile import ZipFile
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -35,6 +36,8 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True, port=5001)
 
 
