7d1f51c104c5c7fe719c3544783f595c2c360d6b
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-08-22 20:30:23 +0200

    Update API.py

--- API/API.py
+++ API/API.py
@@ -4,6 +4,7 @@ import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import logging
+from zipfile import ZipFile
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
