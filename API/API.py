b21e6a4de19f563cfa624b8a7bccc262ce812fe4
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-08-24 12:15:43 +0200

    Update API.py

--- API/API.py
+++ API/API.py
@@ -36,7 +36,7 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 443))
     app.run(host='0.0.0.0', port=port, debug=False) 
 
 
