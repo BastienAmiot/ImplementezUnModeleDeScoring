2c1cb406233d284bf40bb1179fed673c41f9cc78
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-08-26 17:06:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import requests
 import os
-import API import app
+import API
 from zipfile import ZipFile
 
 api_url = "http://localhost:5001/predict"
