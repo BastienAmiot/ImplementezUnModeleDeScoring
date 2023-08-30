332ddabfa924b34ff2f636f41f970098b7ea52ad
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-08-30 11:05:13 +0200

    Update Pytests.py

--- Pytests.py
+++ Pytests.py
@@ -1,1 +1,12 @@
+import API
+import requests
 
+def test_api():
+    response = requests.post('http://localhost:5001/predict', json={"user_id": 456221})
+    assert response.status_code == 200
+    predictions = response.json()
+    assert isinstance(predictions, list)
+    assert len(predictions) > 0
+
+if __name__ == '__main__':
+    test_api()
