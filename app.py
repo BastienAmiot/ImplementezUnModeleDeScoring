b8826db1c3059c69c6c2310d7a07067f32933539
BastienAmiot <139744720+BastienAmiot@users.noreply.github.com>
2023-09-01 23:14:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,8 @@ if predict_button:
     
     with st.sidebar:
       predictions = response
-      st.write('La probabilité que le client soit solvable est de :', str("{:.4f}".format(predictions[0])))
+      st.write(type(predictions))
+      #st.write('La probabilité que le client soit solvable est de :', str("{:.4f}".format(predictions[0])))
       
       if predictions[0] < 0.5:
         st.markdown("""
